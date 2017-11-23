0585a232c7fa4ee741066814a14f50b80df7ff43
clement.wee <clement.wee.hong.en@gmail.com>
2017-11-24 01:39:57 +0800

    add new chloropleth

--- r/chloropleth.R
+++ r/chloropleth.R
@@ -1,25 +1,21 @@
 library(leaflet)
 
-#Read Data
-gdps <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
-countries <- geojsonio::geojson_read("countries.geo.json", what = "sp")
-names(gdps) = c("name", "GDP", "code")
-merged <- sp::merge(countries,gdps)
+make.chloropleth <- function(data, domain, fillC, legendValues){
+  pal <- colorBin("Blues", domain = domain, bins = 5, na.color = "white")
+  
+  m <- leaflet(data) 
+  
+  map = m %>% addPolygons(
+    fillColor = fillC,
+    weight = 2,
+    opacity = 1,
+    color = "black",
+    fillOpacity = 1.0
+  ) %>% addLegend(pal = pal, values = legendValues, opacity = 0.7, title = NULL,
+                  position = "bottomright")
+  
+  map
+}
 
-#Show on Map
 
-pal <- colorBin("Blues", domain = merged$GDP, bins = 5, na.color = "white")
-
-m <- leaflet(merged) 
-
-map = m %>% addPolygons(
-  fillColor = ~pal(GDP),
-  weight = 2,
-  opacity = 1,
-  color = "black",
-  fillOpacity = 1.0
-) %>% addLegend(pal = pal, values = ~GDP, opacity = 0.7, title = NULL,
-               position = "bottomright")
-
-map
   
