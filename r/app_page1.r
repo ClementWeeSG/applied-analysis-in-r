a387c6285cbc9f99754443c89a7426458a79c9d7
clement.wee <clement.wee.hong.en@gmail.com>
2017-11-24 11:43:37 +0800

    add graphs

--- r/app_page1.r
+++ r/app_page1.r
@@ -1,7 +1,32 @@
 # Page 1 renderings
 
+library(ggplot2)
+
 page_1_price_dist <- function(df){
+  # boxplot: standard boxplot.
+  gbox <- ggplot(df,aes(points,price,group=points,fill=country))+geom_boxplot(na.rm=TRUE)
+  
+  
+  # boxplot: standard boxplot.
+  gbox <- ggplot(subset(df, country %in% c("France","Italy","Spain","Chile","United States of America")),
+                 aes(points,fill=country))+
+    geom_bar(na.rm=TRUE,position="dodge")+
+    ggtitle("Bar chart of number of reviews by country")
+  gbox
+}
+
+page_1_variety_dist <- function(df){
   pricingDensity <- density(na.omit(df$price), bw="sj")
   plot(pricingDensity, main = "Distribution of Wine Prices")
   polygon(pricingDensity, col = "blue", border="red")
+}
+
+page_1_country_dist <- function(df){
+  pricingDensity <- density(na.omit(df$price), bw="sj")
+  plot(pricingDensity, main = "Distribution of Wine Prices")
+  polygon(pricingDensity, col = "blue", border="red")
+}
+
+page_1_ratings_dist <- function(df){
+  #TODO ...
 }
