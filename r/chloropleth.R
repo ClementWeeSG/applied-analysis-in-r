90549616c72d24e1e3933c51554f5c7df6adc556
clement.wee <clement.wee.hong.en@gmail.com>
2017-11-24 20:42:01 +0800

    adjust chloropleth

--- r/chloropleth.R
+++ r/chloropleth.R
@@ -5,22 +5,23 @@ wine_file <- read_csv("data/wine_master.utf8.csv")
 mapPolys <- geojsonio::geojson_read("data/countries.geo.json", what = "sp")
 names(mapPolys) = c("id","country")
 
-column = "topic1"
+column = "value1"
 
-  wine_selection = wine_file[, c("recoded_country","price")]
-  names(wine_selection) = c("country","price")
+  wine_selection = wine_file[, c("recoded_country","value11")]
+  names(wine_selection) = c("country","value")
   
-  mapBySelection = aggregate(wine_selection, list( wine_selection$country), median, na.action = na.omit)
+  mapBySelection = aggregate(value ~ country, wine_selection, median, na.action = na.omit)
   merged = sp::merge(mapPolys, mapBySelection)
   color.ramp = colorRamp(c("red","blue"), interpolate = "spline")
-  pal <- colorBin("Blues", domain = merged$price, bins=10, na.color = "white")
+  pal <- colorNumeric(color.ramp, domain = merged$value, na.color = "white")
   
-  leaflet(merged) %>% addPolygons(
-    fillColor = ~pal(price),
+  map = leaflet(merged) %>% addPolygons(
+    fillColor = ~pal(value),
     weight = 2,
     opacity = 1,
     color = "black",
     fillOpacity = 1.0
-  ) %>% addLegend(pal = pal, values = ~price, opacity = 0.7, title = NULL,
+  ) %>% addLegend(pal = pal, values = ~value, opacity = 0.7, title = NULL,
                   position = "bottomright")
   
+map
